ee94b36423721e603816e00623ec03c8c654b56d
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-06-20 17:54:31 +0530

    Create common method to toggle between find and replace panes

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -1,9 +1,13 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import util.FXUtil;
 
@@ -14,14 +18,19 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class EditorFormController {
-    public TextArea txtEditor;
-    public TextField txtSearch;
+    public AnchorPane pneReplace;
     public AnchorPane pneFind;
+    public TextField txtSearchForReplace;
+    public TextField txtReplace;
+    public TextField txtSearch;
+    public TextArea txtEditor;
+    public VBox pneVBox;
     private int findOffset = -1;
     private final List<Index> searchList = new ArrayList<>();
 
     public void initialize() {
         pneFind.setVisible(false);
+        pneReplace.setVisible(false);
 
         txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
             FXUtil.highlightOnTextArea(txtEditor,newValue, Color.web("yellow", 0.8));
@@ -38,9 +47,7 @@ public class EditorFormController {
                 findOffset = -1;
                 txtEditor.deselect();
             } catch (PatternSyntaxException e) {
-
             }
-            System.out.println(searchList);
         });
 
     }
@@ -54,11 +61,21 @@ public class EditorFormController {
     }
 
     public void mnuItemFind_OnAction(ActionEvent actionEvent) {
-        pneFind.setVisible(true);
-        txtSearch.requestFocus();
+        viewSearchOrReplace(pneFind);
     }
 
     public void mnuItemReplace_OnAction(ActionEvent actionEvent) {
+        viewSearchOrReplace(pneReplace);
+    }
+
+    private void viewSearchOrReplace(AnchorPane anchorPane){
+        ObservableList<Node> children = pneVBox.getChildren();
+        if (children.get(0) != anchorPane) {
+            FXCollections.reverse(children);
+        }
+        children.get(0).setVisible(true);
+        children.get(1).setVisible(false);
+        ((AnchorPane)children.get(0)).getChildren().get(0).requestFocus();
     }
 
     public void mnuItemSelectAll_OnAction(ActionEvent actionEvent) {
@@ -83,6 +100,15 @@ public class EditorFormController {
             }
             txtEditor.selectRange(searchList.get(findOffset).startingIndex, searchList.get(findOffset).endIndex);
         }
+    }
+
+    public void btnFind_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceAll_OnAction(ActionEvent actionEvent) {
     }
 }
 
