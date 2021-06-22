9999fdcf23e995554e0bea96c5d74604e3e65bdf
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-06-22 10:24:57 +0530

    Arranged the code (DRY)

--- src/controller/PreferencesFormController.java
+++ src/controller/PreferencesFormController.java
@@ -1,12 +1,12 @@
 package controller;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.RegularExpression;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
@@ -15,7 +15,6 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PreferencesFormController {
@@ -24,57 +23,53 @@ public class PreferencesFormController {
     public ListView<Font> lstFont;
     public TextField txtFontStyle;
     public TextField txtFontSize;
+    public TextField txtSample;
     public TextField txtFont;
-    public Label lblSample;
     EditorFormController editorFormController;
 
     public void initialize() {
         Platform.runLater(() -> {
             editorFormController = (EditorFormController) txtFont.getScene().getUserData();
-            int increment = 1;
-
-            for (int i = 8; i <= 48; i += increment) {
-                lstFontSize.getItems().add(i);
-                if (i == 12) {
-                    increment = 2;
-                } else if (i == 24) {
-                    increment = 4;
-                }
-            }
-
-            lstFontSize.getSelectionModel().select(new Integer((int) editorFormController.txtEditor.getFont().getSize()));
-            txtFontSize.setText((int) editorFormController.txtEditor.getFont().getSize() + "");
-
-            Text regular = new Text("Regular");
-            Text bold = new Text("Bold");
-            bold.setFont(Font.font("System",FontWeight.BOLD,13));
-            Text italic = new Text("Italic");
-            italic.setFont(Font.font("System", FontPosture.ITALIC,13));
-            Text boldItalic = new Text("Bold Italic");
-            boldItalic.setFont(Font.font("System",FontWeight.BOLD,FontPosture.ITALIC,13));
-            lstFontStyle.getItems().setAll(regular, bold, italic, boldItalic);
-
-            if (editorFormController.txtEditor.getFont().getStyle().equals("Regular")) {
-                lstFontStyle.getSelectionModel().select(regular);
-            }else if (editorFormController.txtEditor.getFont().getStyle().equals("Bold")) {
-                lstFontStyle.getSelectionModel().select(bold);
-            }
-            else if (editorFormController.txtEditor.getFont().getStyle().equals("Italic")) {
-                lstFontStyle.getSelectionModel().select(italic);
-            }else{
-                lstFontStyle.getSelectionModel().select(boldItalic);
-            }
-
-            txtFontStyle.setText(lstFontStyle.getSelectionModel().getSelectedItem().getText());
+            loadFontSizes();
+            loadFontStyles();
         });
 
         lstFontSize.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             txtFontSize.setText(newValue + "");
+            setFont(txtSample, Double.parseDouble(txtFontSize.getText()), (txtFontStyle.getText()));
         });
 
         lstFontStyle.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             txtFontStyle.setText(newValue.getText());
+            setFont(txtSample, Double.parseDouble(txtFontSize.getText()), newValue.getText());
         });
+    }
+
+    private void loadFontStyles() {
+        List<String> fontStyleNames = FXCollections.observableArrayList("Regular", "Bold", "Italic", "Bold Italic");
+        ObservableList<Text> fontStyleText = FXCollections.observableArrayList(new Text("Regular"), new Text("Bold"), new Text("Italic"), new Text("Bold Italic"));
+        fontStyleText.get(1).setFont(Font.font("System", FontWeight.BOLD, 13));
+        fontStyleText.get(2).setFont(Font.font("System", FontPosture.ITALIC, 13));
+        fontStyleText.get(3).setFont(Font.font("System", FontWeight.BOLD, FontPosture.ITALIC, 13));
+        lstFontStyle.getItems().setAll(fontStyleText);
+
+        lstFontStyle.getSelectionModel().select(fontStyleNames.indexOf(editorFormController.txtEditor.getFont().getStyle()));
+    }
+
+    private void loadFontSizes() {
+        int increment = 1;
+
+        for (int i = 8; i <= 48; i += increment) {
+            lstFontSize.getItems().add(i);
+            if (i == 12) {
+                increment = 2;
+            } else if (i == 24) {
+                increment = 4;
+            }
+        }
+
+        lstFontSize.getSelectionModel().select(new Integer((int) editorFormController.txtEditor.getFont().getSize()));
+        txtFontSize.setText((int) editorFormController.txtEditor.getFont().getSize() + "");
     }
 
     public void btnResetFont_OnAction(ActionEvent actionEvent) {
@@ -86,15 +81,7 @@ public class PreferencesFormController {
     }
 
     public void btnOk_OnAction(ActionEvent actionEvent) {
-        if (txtFontStyle.getText().equals("Regular")) {
-            editorFormController.txtEditor.setFont(Font.font(Double.parseDouble(txtFontSize.getText())));
-        }else if (txtFontStyle.getText().equals("Bold")) {
-            editorFormController.txtEditor.setFont(Font.font("System",FontWeight.BOLD,Double.parseDouble(txtFontSize.getText())));
-        }else if (txtFontStyle.getText().equals("Italic")) {
-            editorFormController.txtEditor.setFont(Font.font("System",FontWeight.NORMAL,FontPosture.ITALIC,Double.parseDouble(txtFontSize.getText())));
-        }else{
-            editorFormController.txtEditor.setFont(Font.font("System",FontWeight.BOLD,FontPosture.ITALIC,Double.parseDouble(txtFontSize.getText())));
-        }
+        setFont(editorFormController.txtEditor, Double.parseDouble(txtFontSize.getText()), txtFontStyle.getText());
         ((Stage) txtFontSize.getScene().getWindow()).close();
     }
 
@@ -108,6 +95,23 @@ public class PreferencesFormController {
     public void btnCancel_OnKeyPressed(KeyEvent keyEvent) {
         if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
             ((Stage) txtFontSize.getScene().getWindow()).close();
+        }
+    }
+
+    private void setFont(TextInputControl textField, double fontSize, String fontStyle) {
+        switch (fontStyle) {
+            case "Bold":
+                textField.setFont(Font.font("System", FontWeight.BOLD, fontSize));
+                break;
+            case "Italic":
+                textField.setFont(Font.font("System", FontWeight.NORMAL, FontPosture.ITALIC, fontSize));
+                break;
+            case "Bold Italic":
+                textField.setFont(Font.font(fontSize));
+                break;
+            default:
+                textField.setFont(Font.font("System", FontWeight.BOLD, FontPosture.ITALIC, fontSize));
+                break;
         }
     }
 }
